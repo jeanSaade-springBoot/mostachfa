3f2188d815d48ad8a3870cd9f0f5640cfa60f3f7
jean.s <jean.s@euriskomobility.com>
2020-04-25 20:24:50 +0000

    --

--- src/main/java/com/springframework/controllers/ProductController.java
+++ src/main/java/com/springframework/controllers/ProductController.java
@@ -42,7 +42,7 @@ public class ProductController {
     @RequestMapping({"/product/list", "/product"})
     public String listProducts(Model model){
         model.addAttribute("products", productService.listAll());
-        return "html/kkeshLogin";
+        return "html/appointmentMainList";
     }
 
     @RequestMapping("/product/show/{id}")
