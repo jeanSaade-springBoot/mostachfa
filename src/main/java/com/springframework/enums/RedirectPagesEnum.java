da9583a68390cb752b08969367be944bc5ad187a
Micha <micheline.mnassar@gmail.com>
2020-04-28 12:06:46 +0300

    new

--- src/main/java/com/springframework/enums/RedirectPagesEnum.java
+++ src/main/java/com/springframework/enums/RedirectPagesEnum.java
@@ -2,7 +2,7 @@ package com.springframework.enums;
 
 public enum RedirectPagesEnum 
 {
-	    LOGIN("/html/login"),
+	    LOGIN("html/login"),
 	    APPOINTMENT_MAIN_LIST("/html/appointmentMainList"),
 	    MAIN_DETAILS("/html/mainDetails");
 	
